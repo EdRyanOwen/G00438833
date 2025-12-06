7d0c712d750d4901a1519996f8ee12180e612d08
EdRyanOwen <G00438833@atu.ie>
2025-12-06 19:52:19 +0000

    Added Dark Mode toggle feature in Settings

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,27 +1,45 @@
-
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonRadioGroup, IonRadio, IonBackButton, IonButtons } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonRadioGroup, IonRadio, IonBackButton, IonButtons, IonToggle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonRadioGroup, IonRadio, IonBackButton, IonButtons]
+  imports: [CommonModule, FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonRadioGroup, IonRadio, IonBackButton, IonButtons, IonToggle]
 })
 export class SettingsPage implements OnInit {
   measurementUnit: string = 'metric';
+  darkMode: boolean = false;
 
   constructor(private router: Router) {}
 
   ngOnInit() {
     this.measurementUnit = localStorage.getItem('measurementUnit') || 'metric';
+    
+    const savedDarkMode = localStorage.getItem('darkMode');
+    this.darkMode = savedDarkMode === 'true';
+    this.applyDarkMode();
   }
 
   onMeasurementChange() {
     localStorage.setItem('measurementUnit', this.measurementUnit);
+  }
+
+  onDarkModeToggle() {
+    localStorage.setItem('darkMode', this.darkMode.toString());
+    this.applyDarkMode();
+  }
+
+  applyDarkMode() {
+
+    if (this.darkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
   }
 }
